fa4acf4a889aa3994f90f30d2fbf9dc18c22d11e
iheanyi11 <ejeagbaakuegbo@gmail.com>
2025-05-23 13:53:16 +0100

    Downloaded swagger package

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const bodyparser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
 app.use(bodyparser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', require('./routes/index.js'));
 
---
+++ swagger.js
@@ -0,0 +1,18 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Contacts API',
+    description: 'API for managing contacts'
+  },
+    host: 'localhost:3000',
+    schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
+// swaggerAutogen will generate the swagger.json file
+
+
